38e6e4059c3840b9b03039c68fc871d358d94668
carlo-lombardi <lombardi.business@outlook.com>
2023-04-13 21:12:19 +0100

    final exercises clone() from Java

--- src/clone/CloneMethod.java
+++ src/clone/CloneMethod.java
@@ -11,7 +11,7 @@ public class CloneMethod implements Cloneable{//Implement the Cloneable interfac
     @Override//Override the clone method
     public Object clone() {//Override the clone method
         try {//Try to clone the object
-            CloneMethod cm = (CloneMethod) super.clone();//Cast the object to the type of the class
+            CloneMethod cm = (CloneMethod) super.clone();//Cast the object to the type of the class to make sure is the correct class
             cm.name = name;//Assign the value of the variable to the new object
             return cm;//Return the new object
         } catch (CloneNotSupportedException e) {//If the object cannot be cloned
